f947f6988cc938a90832e76c4505096cc8cdaa25
Karin Zohar <karinzohar3@gmail.com>
2023-06-27 10:15:53 +0300

    responsive header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,4 @@
 <script>
-import { RouterLink, RouterView } from "vue-router"
 import { userService } from "../services/user.service"
 import { bitcoinService } from "../services/bitcoin.service"
 import Logo from "./Logo.vue"
@@ -20,8 +19,9 @@ export default {
     isHomePage() {
       return (this.$route.path === '/') ? false : true
     },
-    isMobile() {
-      return (window.innerWidth < 600) ? false : true
+    isWide() {
+      console.log('window.innerWidth:', window.innerWidth > 900)
+      return (window.innerWidth > 900) 
     }
   },
 
@@ -42,7 +42,7 @@ export default {
         <RouterLink to="/contact">Contacts</RouterLink>
         <RouterLink to="/statistics/price-history">Statistics</RouterLink>
       </nav>
-      <div v-if="isHomePage || !isMobile" class="bitcoin-rate">
+      <div v-show="isHomePage && isWide" class="bitcoin-rate">
         <span class="bold">exchange rate</span><span class="light">{{ this.bitcoinRate }}</span>
       </div>
       <div class="user-info">
@@ -64,103 +64,5 @@ export default {
 
 
 <style lang="scss">
-@import "../assets/style/main.scss";
 
-.app-header {
-  padding-block: 1rem;
-  margin-block-end: 1em;
-  z-index: 10;
-  .app-header-content {
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-template-columns: 1fr;
-    width: 100%;
-    height: max-content;
-    align-items: center;
-
-    > * {
-      grid-row: 1;
-      grid-column: 1;
-    }
-
-    .logo-wrapper {
-      align-self: start;
-    }
-    .logo {
-      grid-row: 1;
-      justify-self: start;
-      cursor: pointer;
-    }
-    nav {
-      grid-row: 1;
-      justify-self: center;
-      width: max-content;
-      @include for-normal-layout {
-        flex-direction: row;
-      }
-      flex-direction: column;
-
-      a {
-        font-family: Bellota-bold;
-        font-size: 1.1rem;
-        color: $dark-accent;
-        &:not(:last-of-type) {
-          margin-inline-end: 1em;
-
-        }
-
-        &.router-link-active {
-          color: $accent;
-        }
-
-        &:hover {
-          color: $accent;
-        }
-      }
-    }
-
-    .bitcoin-rate {
-      color: $dark-accent;
-      display: flex;
-      flex-direction: column;
-      text-align: center;
-      width: max-content;
-      border-radius: 4px;
-      justify-self: flex-end;
-      margin-inline-end: 100px;
-      padding: 0.5em;
-    }
-
-    .user-info {
-      justify-self: end;
-
-      .logged-in {
-        .user-img {
-          @include profile-img(48px);
-          cursor: pointer;
-          transition: 0.4s;
-          &:hover {
-            border-color: $accent;
-          }
-        }
-      }
-
-      .logged-out {
-        button {
-          @include button-reset;
-          color: $dark-accent;
-          font-size: 1.1rem;
-          font-family: Bellota-bold;
-        }
-        .signup-btn {
-          margin-inline-end: 1em;
-        }
-
-        .login-btn {
-          @include btn-1($dark-accent);
-        }
-      }
-    }
-  }
-}
 </style>
